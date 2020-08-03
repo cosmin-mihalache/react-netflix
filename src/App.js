66a7bc22174b50d3680cc2e57a6524b404dda4c2
Cosmin <co5myn92@yahoo.com>
2020-08-03 16:34:44 +0300

    initial routes for the app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 function App() {
-  return <h1>Netflix</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/signin">
+          <p>This is the SignIn Page</p>
+        </Route>
+        <Route path="/singout">
+          <p>This is the SignOut Page</p>
+        </Route>
+        <Route path="/browse">
+          <p>This is the Browse Page</p>
+        </Route>
+        <Route path="/">
+          <p>This is the Root Page </p>
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
