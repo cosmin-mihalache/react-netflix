43ab2fc3631ae6c61f056b8785c10e71f7757013
Cosmin <co5myn92@yahoo.com>
2020-08-07 23:15:10 +0300

    adding tests to jumbotron component

---
+++ src/components/jumbotron/__tests__/jumbotron.test.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Jumbotron } from '../../../components';
+import jumbotronData from '../../../fixtures/jumbotron.json';
+
+describe('<Jumbotron />', () => {
+  it('renders the <Jumbotron /> with populated data', () => {
+    const { getByText, getAllByAltText, getByTestId } = render(
+      <Jumbotron.Container>
+        {jumbotronData.map((item) => (
+          <Jumbotron key={item.id} direction={item.direction}>
+            <Jumbotron.Pane>
+              <Jumbotron.Title>{item.title}</Jumbotron.Title>
+              <Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+            </Jumbotron.Pane>
+            <Jumbotron.Pane>
+              <Jumbotron.Image
+                src={item.image}
+                alt={item.alt}
+                data-testid={`${item.id}-jumbotron`}
+              />
+            </Jumbotron.Pane>
+          </Jumbotron>
+        ))}
+      </Jumbotron.Container>
+    );
+
+    expect(getByText('Enjoy on your TV.')).toBeTruthy();
+    expect(getByTestId('1-jumbotron')).toBeTruthy();
+    expect(
+      getByText(
+        'Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.'
+      )
+    ).toBeTruthy();
+    expect(getAllByAltText('Tiger King | Netflix')).toBeTruthy();
+  });
+});
