39f2af69732dafac5ead741447122897bc880180
Cosmin <co5myn92@yahoo.com>
2020-08-08 12:35:38 +0300

    global style with styled-components

---
+++ src/global-styles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+ html, body {
+     font-family: Arial, Helvetica, sans-serif;
+     -webkit-font-smoothing: antialiased;
+     -moz-osx-font-smothing: grayscale;
+     background-color: #000000;
+     color: #333333;
+     font-size: 16px;
+     margin: 0;
+ }
+`;
