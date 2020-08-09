3bed32531be2c2e56954c8df56591e3610dfee5b
Cosmin <co5myn92@yahoo.com>
2020-08-09 10:23:30 +0300

    footer component

---
+++ src/containers/footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Footer } from '../components';
+
+export function FooterContainer() {
+  return (
+    <Footer>
+      <h2>Hello World</h2>
+    </Footer>
+  );
+}
