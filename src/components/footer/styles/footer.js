ba87b1047a94a0350af7c3047356fa7a1c918fb9
Cosmin <co5myn92@yahoo.com>
2020-08-10 20:45:06 +0300

    styled component footer

--- src/components/footer/styles/footer.js
+++ src/components/footer/styles/footer.js
@@ -2,4 +2,55 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
+  max-width: 1000px;
+  flex-direction: column;
+  padding: 70px 50px;
+  margin: auto;
+
+  @media (max-width: 1000px) {
+    padding: 60px 25px;
+  }
+`;
+
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+`;
+
+export const Row = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
+  grid-gap: 10px;
+
+  @media (max-width: 1000px) {
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  }
+`;
+
+export const Title = styled.div`
+  font-size: 1rem;
+  color: #757575;
+  margin-bottom: 40px;
+`;
+
+export const Link = styled.a`
+  color: #757575;
+  margin-bottom: 14px;
+  font-size: 1rem;
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline #757575;
+  }
+`;
+
+export const Break = styled.div`
+  flex-basis: 100%;
+  height: 0;
+`;
+
+export const Text = styled.p`
+  font-size: 0.8rem;
+  color: #757575;
+  margin-bottom: 40px;
 `;
