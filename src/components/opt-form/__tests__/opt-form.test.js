f9a2acd5483636f44d4c76e6ab61e440e57e8102
Cosmin <co5myn92@yahoo.com>
2020-08-24 15:50:34 +0300

    <OptForm /> tests

---
+++ src/components/opt-form/__tests__/opt-form.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { OptForm } from '../../../components';
+
+describe('OptForm', () => {
+  it('renders <OptForm /> component with populated data', () => {
+    const { getByText, getByPlaceholderText } = render(
+      <OptForm>
+        <OptForm.Text>
+          Ready to watch? Enter your email to create or restart your membership.
+        </OptForm.Text>
+        <OptForm.Input placeholder="Email Address" />
+        <OptForm.Button>GET STARTED</OptForm.Button>
+      </OptForm>
+    );
+
+    expect(
+      getByText(
+        'Ready to watch? Enter your email to create or restart your membership.'
+      )
+    ).toBeTruthy();
+    expect(getByPlaceholderText('Email Address')).toBeTruthy();
+    expect(getByText('GET STARTED')).toBeTruthy();
+  });
+});
