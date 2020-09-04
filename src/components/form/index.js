2a448260498531f2c84be74ccb9ff53b29cf1eed
Cosmin <co5myn92@yahoo.com>
2020-09-04 13:25:19 +0300

    structure compound component <Form />

---
+++ src/components/form/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {
+  Container,
+  Base,
+  Title,
+  Submit,
+  Text,
+  SmallText,
+  Input,
+  Link,
+  Error,
+} from './styles/form';
+
+export default function Form({ chidren, ...restProps }) {
+  return <Container {...restProps}>{chidren}</Container>;
+}
+
+Form.Base = function FormBase({ children, ...restProps }) {
+  return <Base {...restProps}>{children}</Base>;
+};
+
+Form.Title = function FormTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Form.Error = function FormError({ children, ...restProps }) {
+  return <Error {...restProps}>{children}</Error>;
+};
+
+Form.Input = function FormInput({ children, ...restProps }) {
+  return <Input {...restProps}>{children}</Input>;
+};
+
+Form.Submit = function FormSubmit({ children, ...restProps }) {
+  return <Submit {...restProps}>{children}</Submit>;
+};
+
+Form.Link = function FormLink({ children, ...restProps }) {
+  return <Link {...restProps}>{children}</Link>;
+};
+
+Form.Text = function FormText({ chilren, ...restProps }) {
+  return <Text {...restProps}>{chilren}</Text>;
+};
+
+Form.SmallText = function FormSmallText({ children, ...restProps }) {
+  return <SmallText {...restProps}>{children}</SmallText>;
+};
