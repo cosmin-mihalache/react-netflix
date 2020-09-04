f9c4853d0b52499c8d682cf4dea95d55f01f152b
Cosmin <co5myn92@yahoo.com>
2020-09-04 15:10:34 +0300

    Sign In page

---
+++ src/pages/signin.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Form } from '../components';
+import { HeaderContainer } from '../containers/header';
+
+export default function SignIn() {
+
+  return (
+    <HeaderContainer>
+      <Form>
+        <Form.Title>Sign In</Form.Title>
+      </Form>
+    </HeaderContainer>
+  );
+}
