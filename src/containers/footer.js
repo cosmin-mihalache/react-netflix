a1ee3ce941875b2306e7b96849d3d0cb9d8cc5a6
Cosmin <co5myn92@yahoo.com>
2020-09-09 12:40:27 +0300

    - changed back to normal - added some links from netflix

--- src/containers/footer.js
+++ src/containers/footer.js
@@ -8,27 +8,28 @@ export function FooterContainer() {
       <Footer.Break />
       <Footer.Row>
         <Footer.Column>
-          <Footer.Link href="#">FAQ</Footer.Link>
-          <Footer.Link href="#">Investor Relations</Footer.Link>
-          <Footer.Link href="#">Privacy</Footer.Link>
-          <Footer.Link href="#">Speed Test</Footer.Link>
+          <Footer.Link href="https://help.netflix.com/en/node/412">FAQ</Footer.Link>
+          <Footer.Link href="https://www.netflixinvestor.com/ir-overview/profile/default.aspx">Investor Relations</Footer.Link>
+          <Footer.Link href="https://help.netflix.com/legal/privacy">Privacy</Footer.Link>
+          <Footer.Link href="https://fast.com/">Speed Test</Footer.Link>
         </Footer.Column>
         <Footer.Column>
-          <Footer.Link href="#">Help Center</Footer.Link>
-          <Footer.Link href="#">Jobs</Footer.Link>
+          <Footer.Link href="https://help.netflix.com/en/">Help Center</Footer.Link>
+          <Footer.Link href="https://jobs.netflix.com/">Jobs</Footer.Link>
           <Footer.Link href="#">Cookie Preferences</Footer.Link>
-          <Footer.Link href="#">Legal Notices</Footer.Link>
+          <Footer.Link href="https://www.netflix.com/ro-en/watch-free">Watch for Free</Footer.Link>
         </Footer.Column>
         <Footer.Column>
           <Footer.Link href="#">Account</Footer.Link>
-          <Footer.Link href="#">Ways to Watch</Footer.Link>
-          <Footer.Link href="#">Corporate Information</Footer.Link>
-          <Footer.Link href="#">Netflix Originals</Footer.Link>
+          <Footer.Link href="https://devices.netflix.com/en/">Ways to Watch</Footer.Link>
+          <Footer.Link href="https://help.netflix.com/en/node/2101">Corporate Information</Footer.Link>
+          <Footer.Link href="https://help.netflix.com/en/legal/notices">Legal Notices</Footer.Link>
         </Footer.Column>
         <Footer.Column>
-          <Footer.Link href="#">Media Center</Footer.Link>
-          <Footer.Link href="#">Terms of Use</Footer.Link>
-          <Footer.Link href="#">Contact Us</Footer.Link>
+          <Footer.Link href="https://media.netflix.com/en/">Media Center</Footer.Link>
+          <Footer.Link href="https://help.netflix.com/en/legal/termsofuse">Terms of Use</Footer.Link>
+          <Footer.Link href="https://help.netflix.com/en/contactus">Contact Us</Footer.Link>
+          <Footer.Link href="https://www.netflix.com/ro-en/browse/genre/839338">Netflix Originals</Footer.Link>
         </Footer.Column>
       </Footer.Row>
       <Footer.Break />
--- src/pages/signin.js
+++ src/pages/signin.js
@@ -17,42 +17,41 @@ export default function SignIn() {
     //if there's error, populate the error state
   };
   return (
-    <HeaderContainer>
-      <Form>
-        <Form.Title>Sign In</Form.Title>
-        {error && <Form.Error>{error}</Form.Error>}
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign In</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
 
-        <Form.Base onSubmit={handleSignIn} method="POST">
-          <Form.Input
-            placeholder="Email address"
-            value={emailAddress}
-            onChange={({ target }) => setEmailAddress(target.value)}
-          />
+          <Form.Base onSubmit={handleSignIn} method="POST">
+            <Form.Input
+              placeholder="Email address"
+              value={emailAddress}
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
 
-          <Form.Input
-            placeholder="Password"
-            type="password"
-            autoCompete="off"
-            value={password}
-            onChange={({ target }) => setPassword(target.value)}
-          />
+            <Form.Input
+              placeholder="Password"
+              type="password"
+              autoCompete="off"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
 
-          <Form.Submit disabled={isInvalid} type="submit">
-            Sign In
-          </Form.Submit>
-          <Form.Text>
-            New to Netflix.<Form.Link to="/signup"> Sign up now.</Form.Link>
-          </Form.Text>
-          <Form.SmallText>
-            This page is protected by Google reCAPTCHA.
-          </Form.SmallText>
-        </Form.Base>
-      </Form>
-      <div>
-        <FooterContainer />
-      </div>
-    </HeaderContainer>
-    
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign In
+            </Form.Submit>
+            <Form.Text>
+              New to Netflix.<Form.Link to="/signup"> Sign up now.</Form.Link>
+            </Form.Text>
+            <Form.SmallText>
+              This page is protected by Google reCAPTCHA.
+            </Form.SmallText>
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
   );
 }
 
