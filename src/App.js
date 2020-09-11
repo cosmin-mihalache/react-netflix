15a4aeae168cec9e5ededa672447344507b4c5ae
Cosmin <co5myn92@yahoo.com>
2020-09-11 13:35:44 +0300

    creating the SignUp page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { Route } from 'react-router-dom';
-import { Home, SignIn } from './pages';
+import { Home, SignIn, SignUp } from './pages';
 import * as ROUTES from './constants/routes';
 
 function App() {
@@ -12,7 +12,7 @@ function App() {
           <SignIn />
         </Route>
         <Route path={ROUTES.SIGN_UP}>
-          <p>This is the SignOut Page</p>
+          <SignUp />
         </Route>
         <Route path={ROUTES.BROWSE}>
           <p>This is the Browse Page</p>
