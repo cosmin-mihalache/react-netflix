15173262875ee4e72e7418385c4d633ae98594b1
Cosmin <co5myn92@yahoo.com>
2020-09-19 15:29:15 +0300

    adding basic Browse page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { Route } from 'react-router-dom';
-import { Home, SignIn, SignUp } from './pages';
+import { Home, SignIn, SignUp, Browse } from './pages';
 import * as ROUTES from './constants/routes';
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
           <SignUp />
         </Route>
         <Route path={ROUTES.BROWSE}>
-          <p>This is the Browse Page</p>
+          <Browse />
         </Route>
         <Route path={ROUTES.HOME}>
           <Home />
