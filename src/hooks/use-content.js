ed910bdddbe0161dd6e69ef2e49ae6b3bbf9674f
Cosmin <co5myn92@yahoo.com>
2020-10-04 21:01:18 +0300

    custom-hook: use-content

---
+++ src/hooks/use-content.js
@@ -0,0 +1,31 @@
+import { useState, useEffect, useContext } from 'react';
+import { FirebaseContext } from '../context/firebase';
+
+/**
+ * need to add comments
+ */
+export default function useContent(target) {
+  const [content, setContent] = useState([]);
+  const { firebase } = useContext(FirebaseContext);
+
+  useEffect(() => {
+    firebase
+      .firestore()
+      .collection(target)
+      .get()
+      .then((snapshot) => {
+        const allContent = snapshot.docs.map((contentObj) => ({
+          ...contentObj.data(),
+          docId: contentObj.id,
+        }));
+
+        setContent(allContent);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+
+  }, [firebase, target]);
+
+  return { [target]: content };
+}
