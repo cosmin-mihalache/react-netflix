5d05c5173f38fc128195d26b66275a53e4f7ff2b
Cosmin <co5myn92@yahoo.com>
2020-10-10 20:56:00 +0300

    Filter movies and series

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { useContent } from '../hooks';
+import selectionFilter from '../utils/selection-filter';
 
 export default function Browse() {
   // need series and the films
-  // slides
-  // pass it to browse container
   const { series } = useContent('series');
   const { films} = useContent('films')
+   //console.log(series);
+   //console.log('films: ', films)
 
+  // slides
+  const slides = selectionFilter({series, films})
+  console.log(slides);
+  // pass it to browse container
  
-   console.log(series);
-   console.log(films)
 
   return <p>This is the Browse Page.</p>;
 }
