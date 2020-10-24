35132504870735cd85926340942d51d128e155f8
Cosmin <co5myn92@yahoo.com>
2020-10-24 14:50:25 +0300

    add profiles container & browse container

--- src/containers/profiles.js
+++ src/containers/profiles.js
@@ -1,24 +1,32 @@
 import React from 'react';
-import { Header } from '../components';
+import { Header, Profiles } from '../components';
 import * as ROUTES from '../constants/routes';
-// import logo from '/images/misc/logo.svg';
 
-export function SelectProfileContainer({ user, SetProfile }) {
+export function SelectProfileContainer({ user, setProfile }) {
+  console.log('user: ', user);
   return (
     <>
-      <Header bg={true}>
-        <Header.Frame></Header.Frame>
-        <Header.Logo
-          to={ROUTES.HOME}
-          src="/images/misc/logo.svg"
-          alt="Netflix"
-        />
+      <Header bg={false}>
+        <Header.Frame>
+          <Header.Logo
+            to={ROUTES.HOME}
+            src="/images/misc/logo.svg"
+            alt="Netflix"
+          />
+        </Header.Frame>
       </Header>
 
       <Profiles>
         <Profiles.Title>Who's Watching?</Profiles.Title>
         <Profiles.List>
-          <Profiles.User>
+          <Profiles.User
+            onClick={() =>
+              setProfile({
+                displayName: user.displayName,
+                photoURL: user.photoURL,
+              })
+            }
+          >
             <Profiles.Picture src={user.photoURL} />
             <Profiles.Name>{user.displayName}</Profiles.Name>
           </Profiles.User>
